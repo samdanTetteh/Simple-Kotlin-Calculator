d5416ad9c5048940ec10510d78eee3e932ffdca1
samdan <samdan@whizzogroup.com>
2020-08-07 10:24:45 +0100

    some idiomatic changes

--- src/main.kt
+++ src/main.kt
@@ -4,17 +4,17 @@ fun main(vararg args: String){
 
     println("Please enter your math problem...")
     var calInput = readLine()
-    while(!calInput.isNullOrBlank()) {
+    while(calInput.isNullOrBlank().not()) {
         println("You entered $calInput")
 
         // Convert user input into array to grab specific values
-        val operation= calInput.split(' ')
+        val operation= calInput?.split(' ')
 
         // Make sure is a valid Math problem
-        if (operation.size == 3) {
+        if (operation?.size == 3) {
 
             // Get Arithmetic operation
-            val operator  = operation[1]
+            val operator  = operation.get(1)
 
             // get operation values and make sure Math operations can be done on them
             val LHS = operation[0].toDoubleOrNull() ?: throw IllegalArgumentException("Invalid input: ${operation[0]}")
